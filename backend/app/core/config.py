5ede22ae85715f76232b05d120de3fb829a5d120
pandey-rakshit <inbox.rakshitpandey@gmail.com>
2026-03-11 14:17:15 +0530

    Vercel Write files

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,7 +1,10 @@
+import os
+
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     allowed_origins: list[str] = ['http://localhost:3000']
+    storage_path: str = "/tmp/tasks.json" if os.getenv("VERCEL") else "storage/tasks.json"
 
     class Config:
         env_file = '.env'
--- backend/app/core/dependencies.py
+++ backend/app/core/dependencies.py
@@ -1,6 +1,8 @@
 from app.repositories.database import TaskRepository
 from app.services.task import TaskService
 
+from app.core.config import settings
+
 
 def get_task_service():
-    return TaskService(repository=TaskRepository())
+    return TaskService(repository=TaskRepository(filepath=settings.storage_path))
